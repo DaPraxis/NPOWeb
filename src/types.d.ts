79b9fa7e26a1f6a0cef32e817abe1a5c22f0d3be
dapraxis <haoyanjiang0709@gmail.com>
2025-03-23 22:06:45 -0400

    deploy

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,16 +1,27 @@
-declare module '*.glb';
-declare module '*.png';
+// declare module '*.glb';
+// declare module '*.png';
 
-declare module 'meshline' {
-  export const MeshLineGeometry: any;
-  export const MeshLineMaterial: any;
-}
+// declare module 'meshline' {
+//   export const MeshLineGeometry: any;
+//   export const MeshLineMaterial: any;
+// }
+
+// declare global {
+//   namespace JSX {
+//     interface IntrinsicElements {
+//       meshLineGeometry: any;
+//       meshLineMaterial: any;
+//     }
+//   }
+// }
+
+/// <reference types="react" />
+/// <reference types="three" />
+
+import { JSX as JSXNamespace } from '@react-three/fiber';
 
 declare global {
   namespace JSX {
-    interface IntrinsicElements {
-      meshLineGeometry: any;
-      meshLineMaterial: any;
-    }
+    interface IntrinsicElements extends JSXNamespace.IntrinsicElements {}
   }
 }
